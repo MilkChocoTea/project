207acc1ff2c03a0dc7434222b8ce4f6c0772d296
MilkChocoTea <37572140+4060E027@users.noreply.github.com>
2025-01-06 15:37:12 +0800

    server_page1修改

--- server/static/page/page1.php
+++ server/static/page/page1.php
@@ -5,7 +5,17 @@ if (!isset($_SESSION['username'])) {
     header("Location: ../../index.php");
     exit();
 }
-
-echo "<h1>歡迎，" . $_SESSION['username'] . "！</h1>";
-echo "<a href='logout.php'>登出</a>";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="../photo/index.ico" type="image/x-icon"/>
+        <title>機械手臂倉儲系統 - 監控系統</title>
+    </head>
+    <body>
+        <?php echo "<h1>歡迎，" . $_SESSION['username'] . "！</h1>"; ?>
+        <a href='logout.php'>登出</a>
+    </body>
+</html>
